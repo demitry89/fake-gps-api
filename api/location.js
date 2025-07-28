5f3ec62d96989fbb45d4d7727f4cde985d558163
demitry89 <danidemitrisukma@gmail.com>
2025-07-28 14:30:26 +0700

    Add location API

--- api/location.js
+++ api/location.js
@@ -1,6 +1,8 @@
-export default function handler(req, res) {
-res.status(200).json({
-lat: -6.24180158483006,
-lon: 106.87688156962395
-});
+function handler(req, res) {
+  res.status(200).json({
+    lat: -6.24180158483006,
+    lon: 106.87688156962395
+  });
 }
+
+module.exports = handler;
