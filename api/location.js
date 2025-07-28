28519974f242c8fa41f5d4b3a1b08651fd70e6bc
demitry89 <danidemitrisukma@gmail.com>
2025-07-28 14:35:41 +0700

    Update location.js

--- api/location.js
+++ api/location.js
@@ -1,8 +1,4 @@
-function handler(req, res) {
-  res.status(200).json({
-    lat: -6.24180158483006,
-    lon: 106.87688156962395
-  });
+export default function handler(req, res) {
+  console.log('API handler dipanggil', req.method);
+  res.status(200).json({ lat: -6.24180158483006, lon: 106.87688156962395 });
 }
-
-module.exports = handler;
